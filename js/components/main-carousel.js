4ab4212f076910319abf7fe66e3f4cfae2032ab4
xcaio <mrlose22@gmail.com>
2022-08-18 22:22:19 -0300

    Create a JavaScript Carousel

---
+++ js/components/main-carousel.js
@@ -0,0 +1,16 @@
+
+
+const btnRight = window.document.querySelector('.button-arrow.-right');
+const btnLeft = window.document.querySelector('.button-arrow.-left');
+const elements = window.document.querySelector('.elements');
+let pixels = 300;
+
+btnRight.addEventListener('click', function(){
+    pixels +=100;
+    elements.style = `transform: translateX(${pixels}px)`;
+});
+
+btnLeft.addEventListener('click', function(){
+    pixels-=100;
+    elements.style = `transform: translateX(${pixels}px)`;
+});
