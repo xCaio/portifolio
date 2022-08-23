2b9a972dbd6a148118f8c6470b8da65e23261b87
xcaio <mrlose22@gmail.com>
2022-08-22 23:18:48 -0300

    Create a e-mail validation

--- js/components/main-contatos.js
+++ js/components/main-contatos.js
@@ -2,23 +2,44 @@ let enviar = document.querySelector('#enviar');
 let email = document.querySelector('#email');
 let message = document.querySelector('#message');
 let nome = document.querySelector('#name');
-
 let alert = document.querySelector('.alert');
+let form = document.querySelector('.main-form');
+
+let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 
-enviar.addEventListener('click',(e) => {
+form.addEventListener('keydown', (e) =>{
+    let text = document.querySelector('.text');
+    if(email.value.match(pattern)){
+        form.classList.add("valid")
+        form.classList.remove("invalid")
+        text.innerHTML = "Seu email é válido";
+        text.style.color = "#62C370"
+        text.style.marginLeft = "50px"
+    }else{
+        form.classList.remove("valid")
+        form.classList.add("invalid")
+        text.innerHTML = "Por favor, insira um e-mail válido";
+        text.style.color = "red"
+        text.style.marginLeft = "50px"
+    }
+    return
+});
+
+
+enviar.addEventListener('click', (e) => {
     e.preventDefault();
 
     const nomeValue = nome.value;
     const emailValue = email.value;
     const messageValue = message.value;
 
-    if(nomeValue === '' || emailValue === '' || messageValue === ''){
+    if (nomeValue === '' || emailValue === '' || messageValue === '') {
         alert.textContent = "Por favor preencha todos os campos!"
         alert.classList = 'error';
 
-       
-    }else{
+
+    } else {
         alert.textContent = "Mensagem Enviada!"
         alert.classList = "success";
     }
@@ -28,7 +49,7 @@ enviar.addEventListener('click',(e) => {
 
     setTimeout(() => {
         alert.textContent = "";
-       alert.classList = ""; 
+        alert.classList = "";
     }, 3000)
     return;
 
